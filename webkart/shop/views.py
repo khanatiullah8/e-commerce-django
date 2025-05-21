e87a37ee1205fa23525e640f76491d43e06b0470
khanatiullah8 <khanatiullah8@gmail.com>
2025-05-21 13:05:53 +0530

    about-us template created

--- webkart/shop/views.py
+++ webkart/shop/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
 
+# home
 def home(request):
     products_all = []
     category_types = Product.objects.values('category').distinct()
@@ -15,8 +16,9 @@ def home(request):
 
     return render(request, 'shop/index.html', params)
 
+# about us
 def about(request):
-    return HttpResponse("About Page")
+    return render(request, 'shop/about.html')
 
 def contact(request):
     return HttpResponse("Contact Page")
@@ -27,6 +29,7 @@ def tracker(request):
 def search(request):
     return HttpResponse("Search Page")
 
+# product view
 def view_product(request, product_id):
     product = Product.objects.filter(id=product_id).first()
 
@@ -34,8 +37,10 @@ def view_product(request, product_id):
 
     return render(request, 'shop/view-product.html', params)
 
+# cart view
 def view_cart(request):
     return render(request, 'shop/view-cart.html')
 
+# checkout
 def checkout(request):
     return HttpResponse("Checkout Page")
