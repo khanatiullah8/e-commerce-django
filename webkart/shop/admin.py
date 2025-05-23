49d0807e840af3b440a34ead27e1b1cae8a3e7e0
khanatiullah8 <khanatiullah8@gmail.com>
2025-05-23 16:30:25 +0530

    cart increment-decrement btn fixes and import-export feature added for product model

--- webkart/shop/admin.py
+++ webkart/shop/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import Product, Contact, Order, OrderUpdate
 
 # Register your models here.
-admin.site.register(Product)
 admin.site.register(Contact)
 admin.site.register(Order)
 admin.site.register(OrderUpdate)
+
+class ProductResource(resources.ModelResource):
+    class Meta:
+        model = Product
+
+class ProductAdmin(ImportExportModelAdmin):
+    resource_classes = [ProductResource]
+
+admin.site.register(Product, ProductAdmin)
